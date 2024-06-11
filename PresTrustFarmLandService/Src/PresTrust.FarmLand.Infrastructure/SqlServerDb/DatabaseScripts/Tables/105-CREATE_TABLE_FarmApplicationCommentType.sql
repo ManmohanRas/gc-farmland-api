26e43cec64ab0d85d112ea1360d68a090f9a2d8b
VinayAdarshRS <vinay@rightanglesol.com>
2024-06-11 06:20:27 -0400

    Application Tables
    Create Tables, Seeds

---
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Infrastructure/SqlServerDb/DatabaseScripts/Tables/105-CREATE_TABLE_FarmApplicationCommentType.sql
@@ -0,0 +1,39 @@
+-- Drop Constraints
+IF OBJECT_ID('[Farm].[FarmApplicationCommentType]') IS NOT NULL
+BEGIN
+	ALTER TABLE [Farm].[FarmApplicationCommentType] DROP CONSTRAINT IF EXISTS  [DF_FarmApplicationCommentType_Description];
+	
+	ALTER TABLE [Farm].[FarmApplicationCommentType] DROP CONSTRAINT IF EXISTS  [DF_FarmApplicationCommentType_IsActive];
+	
+	ALTER TABLE [Farm].[FarmApplicationCommentType] DROP CONSTRAINT IF EXISTS  [FK_ApplicationTypeId_FarmApplicationCommentType]; 
+END;
+GO
+
+-- Drop Table
+DROP TABLE IF EXISTS [Farm].[FarmApplicationCommentType];
+GO
+
+-- Create Table
+CREATE TABLE [Farm].[FarmApplicationCommentType](
+	[Id]				[smallint] 			NOT NULL,
+	[Title]				[varchar](128)		NOT NULL,
+	[Description]		[varchar](512)		NULL,		
+	[IsActive]			[bit]				NULL,
+	[ApplicationTypeId] [smallint]	        NOT NULL,
+CONSTRAINT [PK_FarmApplicationCommentType_Id] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+-- Create Constraints
+ALTER TABLE [Farm].[FarmApplicationCommentType] WITH NOCHECK ADD  CONSTRAINT [DF_FarmApplicationCommentType_Description]  DEFAULT ('') FOR [Description]
+GO 
+ 
+ALTER TABLE [Farm].[FarmApplicationCommentType] WITH NOCHECK ADD  CONSTRAINT [DF_FarmApplicationCommentType_IsActive]  DEFAULT (1) FOR [IsActive]
+GO
+
+ALTER TABLE [Farm].[FarmApplicationCommentType] ADD CONSTRAINT [FK_ApplicationTypeId_FarmApplicationCommentType]  FOREIGN KEY (ApplicationTypeId) REFERENCES [Farm].FarmApplicationType(Id);
+GO 
---
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Infrastructure/SqlServerDb/DatabaseScripts/Tables/203-CREATE_TABLE_FarmApplicationComment.sql
@@ -0,0 +1,38 @@
+IF OBJECT_ID('[Farm].[FarmApplicationComment]') IS NOT NULL
+BEGIN
+	-- Drop Constraints
+	ALTER TABLE [Farm].[FarmApplicationComment] DROP CONSTRAINT IF EXISTS  [FK_ApplicationId_FarmApplicationComment];
+	
+	ALTER TABLE [Farm].[FarmApplicationComment] DROP CONSTRAINT IF EXISTS  [DF_LastUpdatedOn_FarmApplicationComment];
+
+END;
+GO
+  
+-- Drop Table
+DROP TABLE IF EXISTS [Farm].[FarmApplicationComment]
+GO
+
+-- Create Table
+CREATE TABLE [Farm].[FarmApplicationComment](
+	[Id]									[integer] 		IDENTITY(1,1)	NOT NULL,
+	[ApplicationId]							[integer]						NOT NULL,
+	[ApplicationTypeId]						[smallint]						NOT NULL,
+	[Comment]								[varchar](4000)					NOT NULL,
+	[CommentTypeId]							[smallint]						NOT NULL,
+	[LastUpdatedBy]							[varchar](128)					NULL	,
+	[LastUpdatedOn]							[datetime]						NOT NULL,
+
+CONSTRAINT [PK_FarmApplicationComment_Id] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+-- Create Constraints
+ALTER TABLE [Farm].[FarmApplicationComment] ADD CONSTRAINT [FK_ApplicationId_FarmApplicationComment]  FOREIGN KEY (ApplicationId) REFERENCES [Farm].FarmApplication(Id);
+GO 
+
+ALTER TABLE [Farm].[FarmApplicationComment] WITH NOCHECK ADD  CONSTRAINT [DF_LastUpdatedOn_FarmApplicationComment]  DEFAULT (GETDATE()) FOR [LastUpdatedOn]
+GO  
