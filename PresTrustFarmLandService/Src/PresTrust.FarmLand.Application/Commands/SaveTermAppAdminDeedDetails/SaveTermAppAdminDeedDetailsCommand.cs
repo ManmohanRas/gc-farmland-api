fa46824a78c92e4812222d3ff6eac897f2a69313
VinayAdarshRS <vinay@rightanglesol.com>
2024-07-18 06:21:34 -0400

    Farm Admin Deed Details
    Revised Columns ,

--- PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Commands/SaveTermAppAdminDeedDetails/SaveTermAppAdminDeedDetailsCommand.cs
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Commands/SaveTermAppAdminDeedDetails/SaveTermAppAdminDeedDetailsCommand.cs
@@ -9,8 +9,12 @@ public class SaveTermAppAdminDeedDetailsCommand : IRequest<int>
     public string OriginalLot { get; set; }
     public string OriginalBook { get; set; }
     public string OriginalPage { get; set; }
+    public string NOTBlock { get; set; }
+    public string NOTLot { get; set; }
     public string NOTBook { get; set; }
     public string NOTPage { get; set; }
+    public string RDBlock { get; set; }
+    public string RDLot { get; set; }
     public string RDBook { get; set; }
     public string RDPage { get; set; }
     public string LastUpdatedBy { get; set; }
--- PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Queries/GetTermAppAdminDeedDetails/GetTermAppAdminDeedDetailsQueryViewModel.cs
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Queries/GetTermAppAdminDeedDetails/GetTermAppAdminDeedDetailsQueryViewModel.cs
@@ -14,8 +14,12 @@ public class TermAppAdminDeedDetailsViewModel
     public string OriginalLot { get; set; }
     public string OriginalBook { get; set; }
     public string OriginalPage { get; set; }
+    public string NOTBlock { get; set; }
+    public string NOTLot { get; set; }
     public string NOTBook { get; set; }
     public string NOTPage { get; set; }
+    public string RDBlock { get; set; }
+    public string RDLot { get; set; }
     public string RDBook { get; set; }
     public string RDPage { get; set; }
     public string LastUpdatedBy { get; set; }
