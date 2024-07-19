387ebcfb61236eb6b4a8e5c62f8c0fc7557b2f96
saivamshiRas <167420706+saivamshiRas@users.noreply.github.com>
2024-07-19 22:45:52 +0530

    Broken rules
    Broken rules

--- PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Commands/SaveTermAppAdminDeedDetails/SaveTermAppAdminDeedDetailsCommandHandler.cs
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Commands/SaveTermAppAdminDeedDetails/SaveTermAppAdminDeedDetailsCommandHandler.cs
@@ -7,19 +7,26 @@ public class SaveTermAppAdminDeedDetailsCommandHandler : BaseHandler, IRequestHa
     private readonly IPresTrustUserContext userContext;
     private readonly IApplicationRepository repoApplication;
     private readonly ITermAppAdminDeedDetailsRepository repotermAppAdminDeedDetails;
+    private readonly ITermBrokenRuleRepository repoBrokenRules;
+    private readonly SystemParameterConfiguration systemParamOptions;
+
 
     public SaveTermAppAdminDeedDetailsCommandHandler
     (
         IMapper mapper,
         IPresTrustUserContext userContext,
+        IOptions<SystemParameterConfiguration> systemParamOptions,
         IApplicationRepository repoApplication,
-        ITermAppAdminDeedDetailsRepository repotermAppAdminDeedDetails
+        ITermAppAdminDeedDetailsRepository repotermAppAdminDeedDetails,
+        ITermBrokenRuleRepository repoBrokenRules
     ) : base(repoApplication:repoApplication)
     { 
         this.mapper = mapper;
         this.userContext = userContext;
+        this.systemParamOptions = systemParamOptions.Value;
         this.repoApplication = repoApplication;
         this.repotermAppAdminDeedDetails = repotermAppAdminDeedDetails;
+        this.repoBrokenRules = repoBrokenRules;
     }
 
     public async Task<int> Handle(SaveTermAppAdminDeedDetailsCommand request, CancellationToken cancellationToken)
@@ -28,7 +35,173 @@ public class SaveTermAppAdminDeedDetailsCommandHandler : BaseHandler, IRequestHa
         var application = await GetIfApplicationExists(request.ApplicationId);
 
         var reqDeedDetails = mapper.Map<SaveTermAppAdminDeedDetailsCommand, TermAppAdminDeedDetailsEntity>(request);
-        reqDeedDetails = await repotermAppAdminDeedDetails.SaveAsync(reqDeedDetails);
+
+
+        var brokenRules = ReturnBrokenRulesIfAny(application, reqDeedDetails);
+
+        using (var scope = TransactionScopeBuilder.CreateReadCommitted(systemParamOptions.TransScopeTimeOutInMinutes))
+        {
+            await repoBrokenRules.DeleteBrokenRulesAsync(application.Id, ApplicationSectionEnum.ADMIN_DEED_DETAILS);
+            await repoBrokenRules.SaveBrokenRules(brokenRules);
+            reqDeedDetails = await repotermAppAdminDeedDetails.SaveAsync(reqDeedDetails);
+            reqDeedDetails.LastUpdatedBy = userContext.Email;
+            scope.Complete();
+
+        }
+
         return reqDeedDetails.Id;
     }
+
+    private List<TermBrokenRuleEntity> ReturnBrokenRulesIfAny(FarmApplicationEntity application, TermAppAdminDeedDetailsEntity reqDeedDetails)
+    {
+        int sectionId = (int)ApplicationSectionEnum.ADMIN_DEED_DETAILS;
+        List<TermBrokenRuleEntity> brokenRules = new List<TermBrokenRuleEntity>();
+
+        if (string.IsNullOrEmpty(reqDeedDetails.OriginalBlock))
+        {
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Original Block  required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+
+            });
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.OriginalLot))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Original Lot required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.OriginalBook))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Original Book required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.OriginalPage))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Original Page required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.NOTBlock))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Notice Of Termination Block required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.NOTLot))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Notice Of Termination Lot required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.NOTBook))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Notice Of Termination Book required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.NOTPage))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Notice Of Termination Page required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.RDBlock))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Renewal Deed Block required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.RDLot))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Renewal Deed Lot required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.RDBook))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Renewal Deed Book  required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+        if (string.IsNullOrEmpty(reqDeedDetails.RDPage))
+        {
+
+            brokenRules.Add(new TermBrokenRuleEntity()
+            {
+                ApplicationId = application.Id,
+                SectionId = sectionId,
+                Message = "Renewal Deed Page required field on Deed Details Tab have not been filled.",
+                IsApplicantFlow = false
+            });
+
+        }
+
+        return brokenRules;
+    }
+
 }
