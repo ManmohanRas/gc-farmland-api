b3eefefac8835e939c3c152e367b5a3718f27fc7
AparnaRAS <aparna@rightanglesol.com>
2024-09-20 10:41:12 -0400

    View Issue Fix
    View Issue Fix

--- PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Commands/SaveFarmEsmtAttachmentA/SaveFarmEsmtAttachmentACommand.cs
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Commands/SaveFarmEsmtAttachmentA/SaveFarmEsmtAttachmentACommand.cs
@@ -5,7 +5,7 @@ public class SaveFarmEsmtAttachmentACommand : IRequest<int>
     public int Id { get; set; }
     public int ApplicationId { get; set; }
     public bool IsOfferPriceIndicated { get; set; }
-    public bool OfferPriceOpinion { get; set; }
+    public string OfferPriceOpinion { get; set; }
     public int AveragePerAcre { get; set; }
     public string OfferPriceComments { get; set; }
     public string LastUpdatedBy { get; set; }
