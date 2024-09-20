2af2b52fc7434d19b475892264880569375efa72
AparnaRAS <aparna@rightanglesol.com>
2024-09-20 04:37:32 -0400

    US 303 fix

--- PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Queries/GetEsmtAppSignatory/GetEsmtAppSignatoryQueryHandler.cs
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Application/Queries/GetEsmtAppSignatory/GetEsmtAppSignatoryQueryHandler.cs
@@ -26,12 +26,13 @@ public class GetEsmtAppSignatoryQueryHandler : BaseHandler, IRequestHandler<GetE
         // get application details
         var application = await GetIfApplicationExists(request.ApplicationId);
 
-        // get signatory details
         var signatory = await this.repoSignatory.GetSignatoryAsync(request.ApplicationId);
+
         var attachmentAs = await repoAttachmentA.GetEsmtAttachmentAAsync(request.ApplicationId);
+
         var result = mapper.Map<FarmEsmtAppSignatoryEntity, GetEsmtAppSignatoryQueryViewModel>(signatory);
         result.AttachmentAs = mapper.Map<IEnumerable<FarmEsmtAttachmentAEntity>, IEnumerable<FarmEsmtAttachmentAViewModel>>(attachmentAs);
-
+        
         return result;
     }
 }
