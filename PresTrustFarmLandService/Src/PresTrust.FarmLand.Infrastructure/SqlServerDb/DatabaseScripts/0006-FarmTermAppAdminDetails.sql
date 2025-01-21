30ebfdaa901bf168f8f11e9971e9ecd5b1329e87
AparnaRAS <aparna@rightanglesol.com>
2025-01-21 16:27:07 +0530

    Scripts of Admin Tabs.
    Scripts of Admin Tabs.

---
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Infrastructure/SqlServerDb/DatabaseScripts/0006-FarmTermAppAdminDetails.sql
@@ -0,0 +1,64 @@
+﻿
+ BEGIN TRY
+	BEGIN TRANSACTION
+
+	--===================================================================================================================================================
+-- Create Table
+CREATE TABLE [Farm].[FarmTermAppAdminDetails](
+	[Id]							[integer] 		IDENTITY(1,1)	NOT NULL,
+	[ApplicationId]					[integer]						NOT NULL,
+	[SADCId]						[integer]							NULL,
+	[MaxGrant]						[decimal](18,2)						NULL,
+	[PermanentlyPreserved]			[bit]								NULL,
+	[MunicipallyApproved]			[bit]								NULL,
+	[EnrollmentDate]				[dateTime]							NULL,
+	[RenewalDate]					[dateTime]							NULL,
+	[ExpirationDate]				[dateTime]							NULL,
+	[RenewalExpirationDate]			[dateTime]							NULL,
+	[Comment]						[varchar](128)						Null,
+	[LastUpdatedBy]					[varchar](128)						NULL,
+	[LastUpdatedOn]					[datetime]							NULL,
+);
+
+	  INSERT INTO [Farm].[#FarmList]
+		(
+			[ApplicationId],						                                                                                                                                             
+			[SADCId],	                                                                                    
+			[MaxGrant],                                                                                               
+			[PermanentlyPreserved],                                                                                                                                                                
+			[MunicipallyApproved],                        
+			[EnrollmentDate],                      
+			[RenewalDate],                         
+			[ExpirationDate],                       
+			[RenewalExpirationDate],                 
+			[Comment],                         
+			[LastUpdatedBy],	
+			[LastUpdatedOn]
+			)
+            SELECT 
+					[ID],
+					[SADC ID#] AS [SADCId], -- [ID]
+					[Max Grant $] AS [MaxGrant],
+					[Permanently Preserved?] AS [PermanentlyPreserved],
+					[Municipally Approved?] AS [MunicipallyApproved],
+					[Enrollment Date] AS [EnrollmentDate],
+					[Renewal Date] AS [RenewalDate],
+					[Expiration Date] AS [ExpirationDate],
+					[Renew Expir Date] AS [RenewalExpirationDate],
+					[Comment],
+					NULL AS LastUpdatedBy,
+					GetDate() AS [LastUpdatedOn]
+                FROM  [Farm].[TermProgram_Legacy] 
+
+            COMMIT;
+            PRINT 'Term application Admin Details legacy table has been populated';
+END TRY
+BEGIN CATCH
+    DECLARE     @ErrorMessage  NVARCHAR(4000);
+
+    SET         @ErrorMessage = ERROR_MESSAGE();
+    ROLLBACK;
+
+
+    SELECT @ErrorMessage;
+END CATCH
---
+++ PresTrustFarmLandService/Src/PresTrust.FarmLand.Infrastructure/SqlServerDb/DatabaseScripts/0007-FarmTermAppAdminDeedDetails.sql
@@ -0,0 +1,71 @@
+﻿
+ BEGIN TRY
+	BEGIN TRANSACTION
+
+	--===================================================================================================================================================
+-- Drop Table
+DROP TABLE IF EXISTS [Farm].[#FarmTermAppDeedDetails]
+
+
+-- Create Table
+CREATE TABLE [Farm].[#FarmTermAppDeedDetails](
+	[Id]							[integer] 		IDENTITY(1,1)	NOT NULL,
+	[ApplicationId]					[integer]						NOT NULL,
+	[ParcelId]						[integer]						NOT NULL,
+	[NOTBlock]						[varchar](50)					NULL,
+	[NOTLot]						[varchar](50)					NULL,
+	[NOTBook]						[varchar](50)					NULL,
+	[NOTPage]						[varchar](50)					NULL,
+	[RDBlock]						[varchar](50)					NULL,
+	[RDLot]							[varchar](50)					NULL,
+	[RDBook]						[varchar](50)					NULL,
+	[RDPage]						[varchar](50)					NULL,
+	[IsChecked]						[bit]							NOT NULL,
+	[LastUpdatedBy]					[varchar](128)					NULL, 
+	[LastUpdatedOn]					[datetime]						NOT NULL, 
+
+	);
+	        INSERT INTO [Farm].[#FarmTermAppDeedDetails]
+		(
+			[ApplicationId],
+			[ParcelId],
+			[NOTBlock],
+			[NOTLot],
+			[NOTBook],
+			[NOTPage],
+			[RDBlock],
+			[RDLot],
+			[RDBook],
+			[RDPage],
+			[IsChecked],
+			[LastUpdatedBy],
+			[LastUpdatedOn]
+			)
+            SELECT 
+					[ID],
+					NULL AS [ParcelId], -- [ID]
+					NULL AS [NOTBlock],
+					NULL AS [NOTLot],
+					[N-O-T Book] AS [NOTBook],
+					[N-O-T Page] AS [NOTPage],
+					NULL AS [RDBlock],
+					NULL AS [RDLOT],
+					[Renew Book] AS [RDBook],
+					[Renew Page] AS [RDPage],
+					'1' AS [IsChecked],
+					NULL AS [LastUpdatedBy],
+					GetDate() AS [LastUpdatedOn]
+                FROM  [Farm].[TermProgram_Legacy] 
+
+            COMMIT;
+            PRINT 'Term application FarmList legacy table has been populated';
+END TRY
+BEGIN CATCH
+    DECLARE     @ErrorMessage  NVARCHAR(4000);
+
+    SET         @ErrorMessage = ERROR_MESSAGE();
+    ROLLBACK;
+
+
+    SELECT @ErrorMessage;
+END CATCH
